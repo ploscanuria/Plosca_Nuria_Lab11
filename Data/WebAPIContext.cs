0fb6c8550433c7366de6c5ffd9dca6a8f01e46e2
NURIA-LUCIA PLOSCA <nuria.plosca@stud.ubbcluj.ro>
2024-12-22 22:11:22 +0200

    Add project files.

---
+++ Data/WebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Plosca_Nuria_Lab11.Models;
+
+namespace WebAPI.Data
+{
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext (DbContextOptions<WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Plosca_Nuria_Lab11.Models.ShopList> ShopList { get; set; } = default!;
+    }
+}
---
+++ Models/ShopList.cs
@@ -0,0 +1,9 @@
+﻿namespace Plosca_Nuria_Lab11.Models
+{
+    public class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
